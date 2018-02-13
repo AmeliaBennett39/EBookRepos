448ddeb10f6f155389dfcf15cb3398bfa2c55159
MartinaShenoda <martina.f.shenoda@gmail.com>
2018-02-13 12:22:27 -0600

    Added an example of text to speech

--- EBook Console Return/Program.cs
+++ EBook Console Return/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Define;
+using System.Speech;
+using System.Speech.Synthesis;
 
 namespace ConsoleApplication1
 {
@@ -17,8 +19,27 @@ namespace ConsoleApplication1
             definition def = new definition();
             // Make a dynamic object of the definition
             dynamic result = def.getDef(word);
+            
+            
 
             System.Console.WriteLine(result); //def[0]  for 1 definition
+
+            //SpeechSynthesizer reader = new SpeechSynthesizer();
+            //reader.SpeakAsync(result);
+
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+
+                // Configure the audio output. 
+                synth.SetOutputToDefaultAudioDevice();
+
+                // Speak a string synchronously.
+                synth.Speak((string)result[0]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
